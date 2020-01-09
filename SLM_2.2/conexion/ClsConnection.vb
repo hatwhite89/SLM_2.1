511706dda0e82224a48ac6bd0adbdc0967f6edbd
erick javier gallardo almendarez <malvahacker@hotmail.com>
2020-01-09 09:38:24 -0600

    192020

--- SLM_2.2/conexion/ClsConnection.vb
+++ SLM_2.2/conexion/ClsConnection.vb
@@ -7,7 +7,7 @@ Public Class ClsConnection
     Public str_con As String
     'String Conexion
     Public Sub New()
-        str_con = "Data Source=10.172.3.2 ;Initial Catalog=slm_test; Min Pool Size=0;Max Pool Size=1024;Pooling=true; User Id=sa; Password=Lbm2019;"
+        str_con = "Data Source=10.172.3.10 ;Initial Catalog=slm_test; Min Pool Size=0;Max Pool Size=1024;Pooling=true; User Id=sa; Password=Lbm2019;"
     End Sub
 
     Public Function getConexion() As SqlConnection
