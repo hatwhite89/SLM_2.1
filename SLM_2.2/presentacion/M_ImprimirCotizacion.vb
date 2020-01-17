ad6cd873fc812b735436d026472379ee23cd7d4a
software2 <software2@PC-CACX>
2020-01-17 16:26:21 -0600

    viernes 17

--- SLM_2.2/presentacion/M_ImprimirCotizacion.vb
+++ SLM_2.2/presentacion/M_ImprimirCotizacion.vb
@@ -115,6 +115,14 @@ Public Class M_ImprimirCotizacion
             Return Me.DataDefinition.ParameterFields(2)
         End Get
     End Property
+    
+    <Browsable(false),  _
+     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
+    Public ReadOnly Property Parameter_numalet() As CrystalDecisions.[Shared].IParameterField
+        Get
+            Return Me.DataDefinition.ParameterFields(3)
+        End Get
+    End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
