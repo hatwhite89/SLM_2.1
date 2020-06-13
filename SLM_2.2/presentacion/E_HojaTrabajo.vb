d3c15d85f53897278ea5274188cf6377f7d34058
Software 3 <Software 3@DESKTOP-IMTN6BO>
2020-06-13 16:29:21 -0600

    ok

--- SLM_2.2/presentacion/E_HojaTrabajo.vb
+++ SLM_2.2/presentacion/E_HojaTrabajo.vb
@@ -1,5 +1,6 @@
 ﻿Public Class E_HojaTrabajo
     Private Sub E_HojaTrabajo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         txtHora.Text = Date.Now.ToLongTimeString
         txtFecha.Text = Date.Today
     End Sub
