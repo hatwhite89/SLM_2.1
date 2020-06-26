1a04097213156ad956a6a3c4a70d95fe763132b4
Software 3 <Software 3@DESKTOP-IMTN6BO>
2020-06-26 17:46:48 -0600

    ok

--- SLM_2.2/Form1.Designer.vb
+++ SLM_2.2/Form1.Designer.vb
@@ -2390,13 +2390,13 @@ Partial Class Form1
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.gbxMenu)
         Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.PanelFactura)
         Me.Controls.Add(Me.PanelLab)
         Me.Controls.Add(Me.PanelSistema)
         Me.Controls.Add(Me.panelMenu)
         Me.Controls.Add(Me.PanelTalentoHumano)
         Me.Controls.Add(Me.PanelAlmacen)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.PanelFactura)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
