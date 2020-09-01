871c7545753d060c6d9cbbb8fabca8a0b66406d1
Software 3 <Software 3@DESKTOP-IMTN6BO>
2020-09-01 10:30:29 -0600

    ok

--- SLM_2.2/presentacion/E_Usuarios.vb
+++ SLM_2.2/presentacion/E_Usuarios.vb
@@ -90,13 +90,11 @@
             dtUsuarios.DataSource = usuario.listarUsuarios
             dtUsuarios.Columns("codPerfil").Visible = False
 
-
             'bloquear groupbox y campos
             If lblForm.Text = "M_DiarioFacturacion" Then
                 gbxDatos.Enabled = False
                 btnImportar.Enabled = False
             End If
-
 
         Catch ex As Exception
 
@@ -123,8 +121,6 @@
             txtPerfil.Text = row("perfil")
             chkHabilitar.Checked = row("estado")
             lblCodPerfil.Text = row("codPerfil")
-
-
 
         Catch ex As Exception
 
