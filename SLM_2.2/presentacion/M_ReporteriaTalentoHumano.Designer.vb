4ca592948253bbb296bf05be074a35885a916324
software2 <software2@PC-CACX>
2020-11-24 19:10:48 -0600

    reporteria talento humano

--- SLM_2.2/presentacion/M_ReporteriaTalentoHumano.Designer.vb
+++ SLM_2.2/presentacion/M_ReporteriaTalentoHumano.Designer.vb
@@ -27,14 +27,24 @@ Partial Class M_ReporteriaTalentoHumano
         Me.btnGenerar = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmbxReporte = New System.Windows.Forms.ComboBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cmbxHorario = New System.Windows.Forms.ComboBox()
+        Me.lblHorario = New System.Windows.Forms.Label()
+        Me.txtnombreEmpleado = New System.Windows.Forms.TextBox()
+        Me.lblcodeEmpleado = New System.Windows.Forms.Label()
+        Me.lblEmpleado = New System.Windows.Forms.Label()
+        Me.dtpFechaFinal = New System.Windows.Forms.DateTimePicker()
+        Me.lblFechaFinal = New System.Windows.Forms.Label()
+        Me.dtpFechaInicial = New System.Windows.Forms.DateTimePicker()
+        Me.lblFechaInicial = New System.Windows.Forms.Label()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Panel1.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -47,17 +57,18 @@ Partial Class M_ReporteriaTalentoHumano
         Me.Panel1.Controls.Add(Me.btnGenerar)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Location = New System.Drawing.Point(-1, -1)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(1072, 58)
         Me.Panel1.TabIndex = 0
         '
         'btnCerrar
         '
+        Me.btnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnCerrar.BackColor = System.Drawing.Color.White
         Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnCerrar.Location = New System.Drawing.Point(952, 17)
-        Me.btnCerrar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnCerrar.Margin = New System.Windows.Forms.Padding(4)
         Me.btnCerrar.Name = "btnCerrar"
         Me.btnCerrar.Size = New System.Drawing.Size(100, 28)
         Me.btnCerrar.TabIndex = 3
@@ -66,10 +77,11 @@ Partial Class M_ReporteriaTalentoHumano
         '
         'btnGenerar
         '
+        Me.btnGenerar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnGenerar.BackColor = System.Drawing.Color.White
         Me.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnGenerar.Location = New System.Drawing.Point(849, 17)
-        Me.btnGenerar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnGenerar.Margin = New System.Windows.Forms.Padding(4)
         Me.btnGenerar.Name = "btnGenerar"
         Me.btnGenerar.Size = New System.Drawing.Size(100, 28)
         Me.btnGenerar.TabIndex = 2
@@ -90,16 +102,25 @@ Partial Class M_ReporteriaTalentoHumano
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.cmbxReporte)
         Me.GroupBox1.Controls.Add(Me.DataGridView1)
         Me.GroupBox1.Location = New System.Drawing.Point(15, 75)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Size = New System.Drawing.Size(337, 288)
         Me.GroupBox1.TabIndex = 1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Selección de reporte"
+        '
+        'cmbxReporte
+        '
+        Me.cmbxReporte.FormattingEnabled = True
+        Me.cmbxReporte.Items.AddRange(New Object() {"Reporte Personal Activo", "Reporte Personal Inactivo", "Reporte Capacitaciones ", "Reporte Permisos", "Reporte Horarios de Trabajo"})
+        Me.cmbxReporte.Location = New System.Drawing.Point(23, 47)
+        Me.cmbxReporte.Name = "cmbxReporte"
+        Me.cmbxReporte.Size = New System.Drawing.Size(294, 24)
+        Me.cmbxReporte.TabIndex = 1
         '
         'DataGridView1
         '
@@ -107,7 +128,7 @@ Partial Class M_ReporteriaTalentoHumano
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DataGridView1.Location = New System.Drawing.Point(4, 19)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.Size = New System.Drawing.Size(329, 265)
@@ -117,14 +138,109 @@ Partial Class M_ReporteriaTalentoHumano
         '
         Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.cmbxHorario)
+        Me.GroupBox2.Controls.Add(Me.lblHorario)
+        Me.GroupBox2.Controls.Add(Me.txtnombreEmpleado)
+        Me.GroupBox2.Controls.Add(Me.lblcodeEmpleado)
+        Me.GroupBox2.Controls.Add(Me.lblEmpleado)
+        Me.GroupBox2.Controls.Add(Me.dtpFechaFinal)
+        Me.GroupBox2.Controls.Add(Me.lblFechaFinal)
+        Me.GroupBox2.Controls.Add(Me.dtpFechaInicial)
+        Me.GroupBox2.Controls.Add(Me.lblFechaInicial)
         Me.GroupBox2.Location = New System.Drawing.Point(15, 370)
-        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4)
         Me.GroupBox2.Size = New System.Drawing.Size(337, 266)
         Me.GroupBox2.TabIndex = 2
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Campos de busqueda"
+        '
+        'cmbxHorario
+        '
+        Me.cmbxHorario.FormattingEnabled = True
+        Me.cmbxHorario.Location = New System.Drawing.Point(117, 130)
+        Me.cmbxHorario.Name = "cmbxHorario"
+        Me.cmbxHorario.Size = New System.Drawing.Size(200, 24)
+        Me.cmbxHorario.TabIndex = 8
+        Me.cmbxHorario.Visible = False
+        '
+        'lblHorario
+        '
+        Me.lblHorario.AutoSize = True
+        Me.lblHorario.Location = New System.Drawing.Point(46, 130)
+        Me.lblHorario.Name = "lblHorario"
+        Me.lblHorario.Size = New System.Drawing.Size(55, 17)
+        Me.lblHorario.TabIndex = 7
+        Me.lblHorario.Text = "Horario"
+        Me.lblHorario.Visible = False
+        '
+        'txtnombreEmpleado
+        '
+        Me.txtnombreEmpleado.Location = New System.Drawing.Point(117, 57)
+        Me.txtnombreEmpleado.Name = "txtnombreEmpleado"
+        Me.txtnombreEmpleado.ReadOnly = True
+        Me.txtnombreEmpleado.Size = New System.Drawing.Size(200, 22)
+        Me.txtnombreEmpleado.TabIndex = 6
+        Me.txtnombreEmpleado.Visible = False
+        '
+        'lblcodeEmpleado
+        '
+        Me.lblcodeEmpleado.AutoSize = True
+        Me.lblcodeEmpleado.Location = New System.Drawing.Point(30, 46)
+        Me.lblcodeEmpleado.Name = "lblcodeEmpleado"
+        Me.lblcodeEmpleado.Size = New System.Drawing.Size(71, 17)
+        Me.lblcodeEmpleado.TabIndex = 5
+        Me.lblcodeEmpleado.Text = "Empleado"
+        Me.lblcodeEmpleado.Visible = False
+        '
+        'lblEmpleado
+        '
+        Me.lblEmpleado.AutoSize = True
+        Me.lblEmpleado.Location = New System.Drawing.Point(20, 63)
+        Me.lblEmpleado.Name = "lblEmpleado"
+        Me.lblEmpleado.Size = New System.Drawing.Size(71, 17)
+        Me.lblEmpleado.TabIndex = 4
+        Me.lblEmpleado.Text = "Empleado"
+        Me.lblEmpleado.Visible = False
+        '
+        'dtpFechaFinal
+        '
+        Me.dtpFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaFinal.Location = New System.Drawing.Point(117, 160)
+        Me.dtpFechaFinal.Name = "dtpFechaFinal"
+        Me.dtpFechaFinal.Size = New System.Drawing.Size(200, 22)
+        Me.dtpFechaFinal.TabIndex = 3
+        Me.dtpFechaFinal.Visible = False
+        '
+        'lblFechaFinal
+        '
+        Me.lblFechaFinal.AutoSize = True
+        Me.lblFechaFinal.Location = New System.Drawing.Point(20, 160)
+        Me.lblFechaFinal.Name = "lblFechaFinal"
+        Me.lblFechaFinal.Size = New System.Drawing.Size(81, 17)
+        Me.lblFechaFinal.TabIndex = 2
+        Me.lblFechaFinal.Text = "Fecha Final"
+        Me.lblFechaFinal.Visible = False
+        '
+        'dtpFechaInicial
+        '
+        Me.dtpFechaInicial.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaInicial.Location = New System.Drawing.Point(117, 107)
+        Me.dtpFechaInicial.Name = "dtpFechaInicial"
+        Me.dtpFechaInicial.Size = New System.Drawing.Size(200, 22)
+        Me.dtpFechaInicial.TabIndex = 1
+        Me.dtpFechaInicial.Visible = False
+        '
+        'lblFechaInicial
+        '
+        Me.lblFechaInicial.AutoSize = True
+        Me.lblFechaInicial.Location = New System.Drawing.Point(20, 112)
+        Me.lblFechaInicial.Name = "lblFechaInicial"
+        Me.lblFechaInicial.Size = New System.Drawing.Size(83, 17)
+        Me.lblFechaInicial.TabIndex = 0
+        Me.lblFechaInicial.Text = "Fecha Incial"
+        Me.lblFechaInicial.Visible = False
         '
         'GroupBox3
         '
@@ -133,9 +249,9 @@ Partial Class M_ReporteriaTalentoHumano
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBox3.Controls.Add(Me.CrystalReportViewer1)
         Me.GroupBox3.Location = New System.Drawing.Point(360, 75)
-        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4)
         Me.GroupBox3.Size = New System.Drawing.Size(691, 561)
         Me.GroupBox3.TabIndex = 3
         Me.GroupBox3.TabStop = False
@@ -150,20 +266,11 @@ Partial Class M_ReporteriaTalentoHumano
         Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
         Me.CrystalReportViewer1.Location = New System.Drawing.Point(4, 20)
-        Me.CrystalReportViewer1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CrystalReportViewer1.Margin = New System.Windows.Forms.Padding(4)
         Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
         Me.CrystalReportViewer1.Size = New System.Drawing.Size(682, 537)
         Me.CrystalReportViewer1.TabIndex = 0
         Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Reporte Personal Activo", "Reporte Personal Inactivo", "Reporte Capacitaciones ", "Reporte Saldo de Vacaciones", "Reporte Permisos", "Reporte Horarios de Trabajo"})
-        Me.ComboBox1.Location = New System.Drawing.Point(23, 47)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(294, 24)
-        Me.ComboBox1.TabIndex = 1
         '
         'M_ReporteriaTalentoHumano
         '
@@ -175,13 +282,15 @@ Partial Class M_ReporteriaTalentoHumano
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Panel1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "M_ReporteriaTalentoHumano"
         Me.Text = "M_ReporteriaTalentoHumano"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.GroupBox1.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -196,5 +305,14 @@ Partial Class M_ReporteriaTalentoHumano
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents cmbxReporte As ComboBox
+    Friend WithEvents txtnombreEmpleado As TextBox
+    Friend WithEvents lblcodeEmpleado As Label
+    Friend WithEvents lblEmpleado As Label
+    Friend WithEvents dtpFechaFinal As DateTimePicker
+    Friend WithEvents lblFechaFinal As Label
+    Friend WithEvents dtpFechaInicial As DateTimePicker
+    Friend WithEvents lblFechaInicial As Label
+    Friend WithEvents cmbxHorario As ComboBox
+    Friend WithEvents lblHorario As Label
 End Class
--- SLM_2.2/presentacion/M_ReporteriaTalentoHumano.vb
+++ SLM_2.2/presentacion/M_ReporteriaTalentoHumano.vb
@@ -3,4 +3,177 @@
         Me.Close()
 
     End Sub
+
+    Private Sub dtpFechaInicial_MouseDown(sender As Object, e As MouseEventArgs) Handles dtpFechaInicial.MouseDown
+
+        Select Case e.Button
+            Case Windows.Forms.MouseButtons.Left
+                dtpFechaInicial.Format = DateTimePickerFormat.Short
+                dtpFechaInicial.Value = Date.Today
+
+            Case Windows.Forms.MouseButtons.Right
+                dtpFechaInicial.Format = DateTimePickerFormat.Custom
+                dtpFechaInicial.CustomFormat = " "
+        End Select
+
+    End Sub
+    Private Sub dtpFechaFinal_MouseDown(sender As Object, e As MouseEventArgs) Handles dtpFechaFinal.MouseDown
+
+        Select Case e.Button
+            Case Windows.Forms.MouseButtons.Left
+                dtpFechaFinal.Format = DateTimePickerFormat.Short
+                dtpFechaFinal.Value = Date.Today
+
+            Case Windows.Forms.MouseButtons.Right
+                dtpFechaFinal.Format = DateTimePickerFormat.Custom
+                dtpFechaFinal.CustomFormat = " "
+        End Select
+
+    End Sub
+
+    Private Sub cmbxReporte_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbxReporte.SelectedIndexChanged
+        If cmbxReporte.SelectedIndex = 0 Then
+            'Reporte Personal Activo
+            lblEmpleado.Visible = False
+            lblFechaFinal.Visible = False
+            lblFechaInicial.Visible = False
+            lblHorario.Visible = False
+
+            txtnombreEmpleado.Visible = False
+            dtpFechaFinal.Visible = False
+            dtpFechaInicial.Visible = False
+            cmbxHorario.Visible = False
+
+        ElseIf cmbxReporte.SelectedIndex = 1 Then
+            'Reporte Personal Inactivo
+            lblEmpleado.Visible = False
+            lblFechaFinal.Visible = False
+            lblFechaInicial.Visible = False
+            lblHorario.Visible = False
+
+            txtnombreEmpleado.Visible = False
+            dtpFechaFinal.Visible = False
+            dtpFechaInicial.Visible = False
+            cmbxHorario.Visible = False
+        ElseIf cmbxReporte.SelectedIndex = 2 Then
+            'Reporte Capacitaciones
+            lblEmpleado.Visible = False
+            lblFechaFinal.Visible = True
+            lblFechaInicial.Visible = True
+            lblHorario.Visible = False
+
+            txtnombreEmpleado.Visible = False
+            dtpFechaFinal.Visible = True
+            dtpFechaInicial.Visible = True
+            cmbxHorario.Visible = False
+
+            dtpFechaInicial.Value = Date.Now
+            dtpFechaFinal.Value = Date.Now
+        ElseIf cmbxReporte.SelectedIndex = 3 Then
+            'Reporte Permisos
+            lblEmpleado.Visible = True
+            lblFechaFinal.Visible = True
+            lblFechaInicial.Visible = True
+            lblHorario.Visible = False
+
+            txtnombreEmpleado.Visible = True
+            dtpFechaFinal.Visible = True
+            dtpFechaInicial.Visible = True
+            cmbxHorario.Visible = False
+
+            txtnombreEmpleado.Clear()
+
+            dtpFechaInicial.Format = DateTimePickerFormat.Custom
+            dtpFechaInicial.CustomFormat = " "
+
+            dtpFechaFinal.Format = DateTimePickerFormat.Custom
+            dtpFechaFinal.CustomFormat = " "
+
+            dtpFechaFinal.Value = Date.Now
+        ElseIf cmbxReporte.SelectedIndex = 4 Then
+            'Reporte Horarios de Trabajo
+            lblEmpleado.Visible = False
+            lblFechaFinal.Visible = False
+            lblFechaInicial.Visible = False
+            lblHorario.Visible = True
+
+            txtnombreEmpleado.Visible = False
+            dtpFechaFinal.Visible = False
+            dtpFechaInicial.Visible = False
+            cmbxHorario.Visible = True
+
+            llenarHorarios()
+        End If
+    End Sub
+
+    Public Sub llenarHorarios()
+        'llenar el combo box Horarios de Trabajo
+        Try
+            Dim objHora As New ClsHorarios
+            Dim dt As New DataTable
+            dt = objHora.SeleccionarHorario
+
+            cmbxHorario.DataSource = dt
+            cmbxHorario.DisplayMember = "nombre"
+            cmbxHorario.ValueMember = "codigo"
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Validación")
+        End Try
+    End Sub
+
+    Private Sub txtnombreEmpleado_TextChanged(sender As Object, e As EventArgs) Handles txtnombreEmpleado.Click
+        M_ListadoEmpleados.lblform.Text = "M_ReporteriaTalentoHumano"
+        M_ListadoEmpleados.ShowDialog()
+    End Sub
+
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
+        If cmbxReporte.SelectedIndex = 0 Then
+            'Reporte Personal Activo
+            Dim objReporte As New M_CryReportePersonalActivo
+            objReporte.DataSourceConnections.Item(0).SetLogon("sa", "Lbm2019")
+            CrystalReportViewer1.ReportSource = objReporte
+        ElseIf cmbxReporte.SelectedIndex = 1 Then
+            'Reporte Personal Inactivo
+            Dim objReporte As New M_CryReportePersonalInactivo
+            objReporte.DataSourceConnections.Item(0).SetLogon("sa", "Lbm2019")
+            CrystalReportViewer1.ReportSource = objReporte
+        ElseIf cmbxReporte.SelectedIndex = 2 Then
+            'Reporte Capacitaciones
+            Dim objReporte As New M_CryReporteCapacitaciones
+            If Trim(dtpFechaInicial.Text) <> "" And Trim(dtpFechaFinal.Text) <> "" Then
+                objReporte.SetParameterValue("@fechaInicial", dtpFechaInicial.Value)
+                objReporte.SetParameterValue("@fechaFinal", dtpFechaFinal.Value)
+            Else
+                MsgBox("Debe ingresar la fecha inicial y la fecha final.", MsgBoxStyle.Information, "Validación.")
+                Exit Sub
+            End If
+            objReporte.DataSourceConnections.Item(0).SetLogon("sa", "Lbm2019")
+            CrystalReportViewer1.ReportSource = objReporte
+        ElseIf cmbxReporte.SelectedIndex = 3 Then
+            'Reporte Permisos
+            Dim objReporte As New M_CryReportePermisosXEmpleadoYFechas
+            If Trim(txtnombreEmpleado.Text) <> "" Then
+                objReporte.SetParameterValue("@codigoEmpleado", Convert.ToInt64(lblcodeEmpleado.Text))
+            Else
+                MsgBox("Vacio empleado")
+                objReporte.SetParameterValue("@codigoEmpleado", Nothing)
+            End If
+            If Trim(dtpFechaInicial.Text) <> "" And Trim(dtpFechaFinal.Text) <> "" Then
+                objReporte.SetParameterValue("@fechaInicial", dtpFechaInicial.Value)
+                objReporte.SetParameterValue("@fechaFinal", dtpFechaFinal.Value)
+            Else
+                MsgBox("Vacio fechas")
+                objReporte.SetParameterValue("@fechaInicial", Nothing)
+                objReporte.SetParameterValue("@fechaFinal", Nothing)
+            End If
+            objReporte.DataSourceConnections.Item(0).SetLogon("sa", "Lbm2019")
+            CrystalReportViewer1.ReportSource = objReporte
+        ElseIf cmbxReporte.SelectedIndex = 4 Then
+            'Reporte Horarios de Trabajo
+            Dim objReporte As New M_CryReporteHorarioTrabajo
+            objReporte.SetParameterValue("@codigoHorario", Convert.ToInt64(cmbxHorario.SelectedValue))
+            objReporte.DataSourceConnections.Item(0).SetLogon("sa", "Lbm2019")
+            CrystalReportViewer1.ReportSource = objReporte
+        End If
+    End Sub
 End Class
