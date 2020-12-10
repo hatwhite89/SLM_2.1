b5eb85b47b3601a8fcd23322df01a97d2413088c
Software 3 <Software 3@DESKTOP-IMTN6BO>
2020-12-10 12:52:15 -0600

    ok

---
+++ SLM_2.2/negocio/ClsTomaDeMuestras.vb
@@ -0,0 +1,136 @@
+﻿
+
+Imports System.Data.SqlClient
+Public Class ClsTomaDeMuestras
+
+    Dim codMuestra, codExamen As Integer
+    Dim descripcion As String
+
+    'Constructor
+    Public Sub New()
+
+    End Sub
+
+    Public Property codMuestra_ As Integer
+        Get
+            Return codMuestra
+        End Get
+        Set(value As Integer)
+            codMuestra = value
+        End Set
+    End Property
+
+    Public Property codExamen_ As Integer
+        Get
+            Return codExamen
+        End Get
+        Set(value As Integer)
+            codExamen = value
+        End Set
+    End Property
+
+    Public Property Descripcion_ As String
+        Get
+            Return descripcion
+        End Get
+        Set(value As String)
+            descripcion = value
+        End Set
+    End Property
+
+
+
+    Public Function registrarNuevo() As String
+        Dim sqlcom As SqlCommand
+        Dim sqlpar As SqlParameter
+        Dim par_sal As Integer
+
+        sqlcom = New SqlCommand
+        sqlcom.CommandType = CommandType.StoredProcedure
+        sqlcom.CommandText = "A_slmInsertarTomaDeMuestras"
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "codExamen" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = codExamen_
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "descripcion" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Descripcion_
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "ID"
+        sqlpar.Value = ""
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar.Direction = ParameterDirection.Output
+
+        Dim con As New ClsConnection
+        sqlcom.Connection = con.getConexion
+
+        sqlcom.ExecuteNonQuery()
+
+        con.cerrarConexion()
+
+        par_sal = sqlcom.Parameters("ID").Value
+
+        Return par_sal
+
+    End Function
+
+
+
+
+
+
+
+    Public Function modificarRegistro() As String
+        Dim sqlcom As SqlCommand
+        Dim sqlpar As SqlParameter
+        Dim par_sal As Integer
+
+        sqlcom = New SqlCommand
+        sqlcom.CommandType = CommandType.StoredProcedure
+        sqlcom.CommandText = "A_slmActualizarTomaDeMuestras"
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "codMuestra" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = codMuestra_
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "codExamen" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = codExamen_
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "descripcion" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Descripcion_
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "salida"
+        sqlpar.Value = ""
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar.Direction = ParameterDirection.Output
+
+        Dim con As New ClsConnection
+        sqlcom.Connection = con.getConexion
+
+        sqlcom.ExecuteNonQuery()
+
+        con.cerrarConexion()
+
+        par_sal = sqlcom.Parameters("salida").Value
+
+        Return par_sal
+
+    End Function
+
+
+
+
+
+End Class
---
+++ SLM_2.2/presentacion/MM_TomaDeMuestras.Designer.vb
@@ -0,0 +1,356 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MM_TomaDeMuestras
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MM_TomaDeMuestras))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnCrear = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.txtCodExamen = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtExamen = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Cod = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.codMuestra = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.enunciado = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SI = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.NO = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Nota = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lblform = New System.Windows.Forms.Label()
+        Me.lblcodexamen = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.btnGuardar)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.btnModificar)
+        Me.Panel1.Controls.Add(Me.btnCrear)
+        Me.Panel1.Controls.Add(Me.btnCerrar)
+        Me.Panel1.Location = New System.Drawing.Point(1, 1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(696, 43)
+        Me.Panel1.TabIndex = 23
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGuardar.BackColor = System.Drawing.Color.LawnGreen
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.Location = New System.Drawing.Point(530, 10)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 2
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(11, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(218, 24)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Cuestionario de Exámen"
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnModificar.BackColor = System.Drawing.Color.Yellow
+        Me.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModificar.Location = New System.Drawing.Point(449, 10)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 1
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = False
+        '
+        'btnCrear
+        '
+        Me.btnCrear.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCrear.BackColor = System.Drawing.Color.White
+        Me.btnCrear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCrear.Location = New System.Drawing.Point(363, 10)
+        Me.btnCrear.Name = "btnCrear"
+        Me.btnCrear.Size = New System.Drawing.Size(80, 23)
+        Me.btnCrear.TabIndex = 0
+        Me.btnCrear.Text = "Crear Nuevo"
+        Me.btnCrear.UseVisualStyleBackColor = False
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCerrar.BackColor = System.Drawing.Color.LightSalmon
+        Me.btnCerrar.FlatAppearance.BorderSize = 0
+        Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCerrar.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnCerrar.Location = New System.Drawing.Point(610, 10)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCerrar.TabIndex = 17
+        Me.btnCerrar.Text = "Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox1.Controls.Add(Me.lblcodexamen)
+        Me.GroupBox1.Controls.Add(Me.lblform)
+        Me.GroupBox1.Controls.Add(Me.txtExamen)
+        Me.GroupBox1.Controls.Add(Me.txtDescripcion)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtCodExamen)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtCodigo)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 50)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(671, 101)
+        Me.GroupBox1.TabIndex = 24
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Información"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(560, 11)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(81, 81)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cód."
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(91, 31)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodigo.TabIndex = 2
+        '
+        'txtCodExamen
+        '
+        Me.txtCodExamen.Location = New System.Drawing.Point(250, 30)
+        Me.txtCodExamen.Name = "txtCodExamen"
+        Me.txtCodExamen.Size = New System.Drawing.Size(59, 20)
+        Me.txtCodExamen.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(203, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Exámen"
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtDescripcion.Location = New System.Drawing.Point(91, 57)
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(422, 20)
+        Me.txtDescripcion.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Descripción"
+        '
+        'txtExamen
+        '
+        Me.txtExamen.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtExamen.Location = New System.Drawing.Point(315, 30)
+        Me.txtExamen.Name = "txtExamen"
+        Me.txtExamen.Size = New System.Drawing.Size(198, 20)
+        Me.txtExamen.TabIndex = 7
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 157)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(671, 321)
+        Me.GroupBox2.TabIndex = 25
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Preguntas de Exámen"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Cod, Me.codMuestra, Me.enunciado, Me.SI, Me.NO, Me.Nota})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 16)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(665, 302)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Cod
+        '
+        Me.Cod.HeaderText = "CodPregunta"
+        Me.Cod.Name = "Cod"
+        Me.Cod.Visible = False
+        '
+        'codMuestra
+        '
+        Me.codMuestra.HeaderText = "codMuestra"
+        Me.codMuestra.Name = "codMuestra"
+        Me.codMuestra.Visible = False
+        '
+        'enunciado
+        '
+        Me.enunciado.FillWeight = 224.3952!
+        Me.enunciado.HeaderText = "Enunciado"
+        Me.enunciado.Name = "enunciado"
+        '
+        'SI
+        '
+        Me.SI.FillWeight = 34.78343!
+        Me.SI.HeaderText = "SI"
+        Me.SI.Name = "SI"
+        Me.SI.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.SI.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'NO
+        '
+        Me.NO.FillWeight = 39.29848!
+        Me.NO.HeaderText = "NO"
+        Me.NO.Name = "NO"
+        Me.NO.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.NO.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'Nota
+        '
+        Me.Nota.FillWeight = 101.5228!
+        Me.Nota.HeaderText = "Nota"
+        Me.Nota.Name = "Nota"
+        '
+        'lblform
+        '
+        Me.lblform.AutoSize = True
+        Me.lblform.Location = New System.Drawing.Point(473, 81)
+        Me.lblform.Name = "lblform"
+        Me.lblform.Size = New System.Drawing.Size(37, 13)
+        Me.lblform.TabIndex = 8
+        Me.lblform.Text = "lblform"
+        '
+        'lblcodexamen
+        '
+        Me.lblcodexamen.AutoSize = True
+        Me.lblcodexamen.Location = New System.Drawing.Point(312, 14)
+        Me.lblcodexamen.Name = "lblcodexamen"
+        Me.lblcodexamen.Size = New System.Drawing.Size(72, 13)
+        Me.lblcodexamen.TabIndex = 9
+        Me.lblcodexamen.Text = "lblcodexamen"
+        Me.lblcodexamen.Visible = False
+        '
+        'MM_TomaDeMuestras
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(695, 487)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "MM_TomaDeMuestras"
+        Me.Text = "Toma de Muestras"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnCrear As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtExamen As TextBox
+    Friend WithEvents txtDescripcion As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCodExamen As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Cod As DataGridViewTextBoxColumn
+    Friend WithEvents codMuestra As DataGridViewTextBoxColumn
+    Friend WithEvents enunciado As DataGridViewTextBoxColumn
+    Friend WithEvents SI As DataGridViewCheckBoxColumn
+    Friend WithEvents NO As DataGridViewCheckBoxColumn
+    Friend WithEvents Nota As DataGridViewTextBoxColumn
+    Friend WithEvents lblform As Label
+    Friend WithEvents lblcodexamen As Label
+End Class
