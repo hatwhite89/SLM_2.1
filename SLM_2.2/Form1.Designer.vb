0610d4af0a46a4f54f89e536e92fd53f971b515a
hatwhite88 <erickgallardo89@yahoo.com>
2020-12-15 13:55:47 -0600

    si

--- SLM_2.2/Form1.Designer.vb
+++ SLM_2.2/Form1.Designer.vb
@@ -2362,6 +2362,7 @@ Partial Class Form1
         Me.Controls.Add(Me.PanelLab)
         Me.Controls.Add(Me.Panel1)
         Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Menú Principal"
