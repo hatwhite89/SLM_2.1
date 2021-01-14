e75277f70e26249c637c165df6931725109a677c
Software 3 <Software 3@DESKTOP-IMTN6BO>
2021-01-14 13:31:42 -0600

    ok

--- SLM_2.2/presentacion/E_DetalleExamenes.vb
+++ SLM_2.2/presentacion/E_DetalleExamenes.vb
@@ -460,7 +460,7 @@
 
     End Sub
 
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) 
         E_Unidad.Show()
     End Sub
 End Class
