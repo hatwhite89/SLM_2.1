d7b3e95d0b1cdee77f12620fabf77391a87b86ef
hatwhite89 <gallardoerick89@gmail.com>
2021-03-29 10:50:36 -0600

    5lkll

---
+++ SLM_2.2/negocio/clsTomaDeMuestra.vb
@@ -0,0 +1,199 @@
+﻿Imports System.Data.SqlClient
+
+Public Class clsTomaDeMuestra
+    Dim factura As String
+    Dim usuario, pass, estacion, sucursal As String
+    Dim id_sucursal, id_impresora As Integer
+    Public Property Factura1 As String
+        Get
+            Return factura
+        End Get
+        Set(value As String)
+            factura = value
+        End Set
+    End Property
+
+    Public Property Usuario1 As String
+        Get
+            Return usuario
+        End Get
+        Set(value As String)
+            usuario = value
+        End Set
+    End Property
+
+    Public Property Pass1 As String
+        Get
+            Return pass
+        End Get
+        Set(value As String)
+            pass = value
+        End Set
+    End Property
+
+    Public Property Estacion1 As String
+        Get
+            Return estacion
+        End Get
+        Set(value As String)
+            estacion = value
+        End Set
+    End Property
+
+    Public Property Sucursal1 As String
+        Get
+            Return sucursal
+        End Get
+        Set(value As String)
+            sucursal = value
+        End Set
+    End Property
+
+    Public Property Id_sucursal1 As Integer
+        Get
+            Return id_sucursal
+        End Get
+        Set(value As Integer)
+            id_sucursal = value
+        End Set
+    End Property
+
+    Public Property Id_impresora1 As Integer
+        Get
+            Return id_impresora
+        End Get
+        Set(value As Integer)
+            id_impresora = value
+        End Set
+    End Property
+
+    Public Function RecuperarPreguntas(id As Integer) As SqlDataReader
+        Dim sqlcom As SqlCommand
+        sqlcom = New SqlCommand
+        sqlcom.CommandText = "select pregunta,valor,nota from respuestasTomaMuestra where id_factura = '" + id.ToString + "'"
+        sqlcom.Connection = New ClsConnection().getConexion
+
+        Return sqlcom.ExecuteReader
+
+    End Function
+    Public Function ListarSucursales() As DataTable
+
+        Dim objCon As New ClsConnection
+        Dim cn As New SqlConnection
+        cn = objCon.getConexion
+
+        Using da As New SqlDataAdapter("select * from Sucursal", cn)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            Return dt
+        End Using
+    End Function
+    Public Function ListarUsuarios() As DataTable
+
+        Dim objCon As New ClsConnection
+        Dim cn As New SqlConnection
+        cn = objCon.getConexion
+
+        Using da As New SqlDataAdapter("select u.id,u.usuario,u.fecha_registro,u.estacion,u.sucursal,i.alias from UsuariosTM u, impresoras i
+where i.sucursal = u.id_sucursal", cn)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            Return dt
+        End Using
+    End Function
+
+    Public Function RegistrarUsuario() As String
+        Dim sqlcom As SqlCommand
+        Dim sqlpar As SqlParameter
+        Dim par_sal As Integer
+
+        sqlcom = New SqlCommand
+        sqlcom.CommandType = CommandType.StoredProcedure
+        sqlcom.CommandText = "E_slmRegistrarUsuarioTM"
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "usuario" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Usuario1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "pass" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Pass1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "estacion" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Estacion1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "id_sucusal" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Id_sucursal1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "sucursal" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Sucursal1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "impresora" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Id_impresora1
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "salida"
+        sqlpar.Value = ""
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar.Direction = ParameterDirection.Output
+
+        Dim con As New ClsConnection
+        sqlcom.Connection = con.getConexion
+
+        sqlcom.ExecuteNonQuery()
+
+        con.cerrarConexion()
+
+        par_sal = sqlcom.Parameters("salida").Value
+
+        Return par_sal
+
+    End Function
+
+    Public Function EliminarUsuario() As String
+        Dim sqlcom As SqlCommand
+        Dim sqlpar As SqlParameter
+        Dim par_sal As Integer
+
+        sqlcom = New SqlCommand
+        sqlcom.CommandType = CommandType.StoredProcedure
+        sqlcom.CommandText = "E_slmEliminarUsuarioTM"
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "usuario" 'nombre campo en el procedimiento almacenado @
+        sqlpar.Value = Usuario1
+        sqlcom.Parameters.Add(sqlpar)
+
+
+
+        sqlpar = New SqlParameter
+        sqlpar.ParameterName = "salida"
+        sqlpar.Value = ""
+        sqlcom.Parameters.Add(sqlpar)
+
+        sqlpar.Direction = ParameterDirection.Output
+
+        Dim con As New ClsConnection
+        sqlcom.Connection = con.getConexion
+
+        sqlcom.ExecuteNonQuery()
+
+        con.cerrarConexion()
+
+        par_sal = sqlcom.Parameters("salida").Value
+
+        Return par_sal
+
+    End Function
+End Class
---
+++ SLM_2.2/presentacion/E_frmConfiguracionCorreoResultado.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class E_frmConfiguracionCorreoResultado
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(E_frmConfiguracionCorreoResultado))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtHost = New System.Windows.Forms.TextBox()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.txtPuerto = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.rsbVerdadero = New System.Windows.Forms.RadioButton()
+        Me.rbdFalso = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 54)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(3, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(300, 31)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Entrega de resultados"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.txtPuerto)
+        Me.GroupBox1.Controls.Add(Me.txtPass)
+        Me.GroupBox1.Controls.Add(Me.txtHost)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtCorreo)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 60)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(778, 174)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Correo"
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(22, 40)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(205, 20)
+        Me.txtCorreo.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Correo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Pass"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(265, 21)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Host"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(265, 97)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Puerto"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(514, 21)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 13)
+        Me.Label6.TabIndex = 5
+        '
+        'txtHost
+        '
+        Me.txtHost.Location = New System.Drawing.Point(268, 40)
+        Me.txtHost.Name = "txtHost"
+        Me.txtHost.Size = New System.Drawing.Size(205, 20)
+        Me.txtHost.TabIndex = 6
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(22, 113)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(205, 20)
+        Me.txtPass.TabIndex = 7
+        '
+        'txtPuerto
+        '
+        Me.txtPuerto.Location = New System.Drawing.Point(268, 113)
+        Me.txtPuerto.Name = "txtPuerto"
+        Me.txtPuerto.Size = New System.Drawing.Size(205, 20)
+        Me.txtPuerto.TabIndex = 8
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rbdFalso)
+        Me.GroupBox2.Controls.Add(Me.rsbVerdadero)
+        Me.GroupBox2.Location = New System.Drawing.Point(517, 37)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(252, 93)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "SSL"
+        '
+        'rsbVerdadero
+        '
+        Me.rsbVerdadero.AutoSize = True
+        Me.rsbVerdadero.Location = New System.Drawing.Point(21, 31)
+        Me.rsbVerdadero.Name = "rsbVerdadero"
+        Me.rsbVerdadero.Size = New System.Drawing.Size(74, 17)
+        Me.rsbVerdadero.TabIndex = 0
+        Me.rsbVerdadero.TabStop = True
+        Me.rsbVerdadero.Text = "Verdadero"
+        Me.rsbVerdadero.UseVisualStyleBackColor = True
+        '
+        'rbdFalso
+        '
+        Me.rbdFalso.AutoSize = True
+        Me.rbdFalso.Location = New System.Drawing.Point(21, 64)
+        Me.rbdFalso.Name = "rbdFalso"
+        Me.rbdFalso.Size = New System.Drawing.Size(50, 17)
+        Me.rbdFalso.TabIndex = 1
+        Me.rbdFalso.TabStop = True
+        Me.rbdFalso.Text = "Falso"
+        Me.rbdFalso.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(644, 136)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 32)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Guardar Cambios"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 243)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'E_frmConfiguracionCorreoResultado
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(800, 265)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "E_frmConfiguracionCorreoResultado"
+        Me.Text = "E_frmConfiguracionCorreoResultado"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtHost As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents rbdFalso As RadioButton
+    Friend WithEvents rsbVerdadero As RadioButton
+    Friend WithEvents txtPuerto As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+End Class
