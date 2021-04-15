ec5ae559c643ea64ef1264ad3e225f1c8801deb3
hatwhite89 <gallardoerick89@gmail.com>
2021-04-15 11:23:33 -0600

    aa

---
+++ SLM_2.2/presentacion/M_ImportarListasPrecios.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class M_ImportarListasPrecios
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.dtExcel = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblCantidadR = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dtExcel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox1.Controls.Add(Me.btnCancelar)
+        Me.GroupBox1.Controls.Add(Me.btnModificar)
+        Me.GroupBox1.Controls.Add(Me.btnGuardar)
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(702, 78)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Acciones"
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.Color.LightSalmon
+        Me.btnCancelar.FlatAppearance.BorderSize = 0
+        Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancelar.ForeColor = System.Drawing.Color.White
+        Me.btnCancelar.Location = New System.Drawing.Point(204, 28)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 10
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnModificar
+        '
+        Me.btnModificar.BackColor = System.Drawing.Color.LightSalmon
+        Me.btnModificar.FlatAppearance.BorderSize = 0
+        Me.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModificar.ForeColor = System.Drawing.Color.White
+        Me.btnModificar.Location = New System.Drawing.Point(39, 28)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 9
+        Me.btnModificar.Text = "Importar"
+        Me.btnModificar.UseVisualStyleBackColor = False
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.BackColor = System.Drawing.Color.LightSalmon
+        Me.btnGuardar.FlatAppearance.BorderSize = 0
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.ForeColor = System.Drawing.Color.White
+        Me.btnGuardar.Location = New System.Drawing.Point(123, 28)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 8
+        Me.btnGuardar.Text = "Agregar"
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'dtExcel
+        '
+        Me.dtExcel.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dtExcel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dtExcel.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dtExcel.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.dtExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtExcel.Location = New System.Drawing.Point(8, 86)
+        Me.dtExcel.Name = "dtExcel"
+        Me.dtExcel.RowHeadersWidth = 51
+        Me.dtExcel.Size = New System.Drawing.Size(702, 443)
+        Me.dtExcel.TabIndex = 9
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.lblCantidadR)
+        Me.Panel1.Location = New System.Drawing.Point(8, 535)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(702, 34)
+        Me.Panel1.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Cantidad de Registros:"
+        '
+        'lblCantidadR
+        '
+        Me.lblCantidadR.AutoSize = True
+        Me.lblCantidadR.Location = New System.Drawing.Point(123, 10)
+        Me.lblCantidadR.Name = "lblCantidadR"
+        Me.lblCantidadR.Size = New System.Drawing.Size(11, 15)
+        Me.lblCantidadR.TabIndex = 6
+        Me.lblCantidadR.Text = "-"
+        '
+        'M_ImportarListasPrecios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(718, 571)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.dtExcel)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.Name = "M_ImportarListasPrecios"
+        Me.Text = "Importar Listas de Precios"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.dtExcel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents dtExcel As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblCantidadR As Label
+End Class
---
+++ SLM_2.2/presentacion/M_ImportarListasPrecios.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Data.OleDb
+
+Public Class M_ImportarListasPrecios
+
+    Private Sub M_ImportarListasPrecios_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If (e.KeyCode = Keys.Escape) Then
+            Me.Close()
+        End If
+    End Sub
+    Sub importarExcel2(ByVal tabla As DataGridView)
+        Dim myFileDialog As New OpenFileDialog()
+        Dim xSheet As String = ""
+
+        With myFileDialog
+            .Filter = "Excel Files |*.xlsx"
+            .Title = "Open File"
+            .ShowDialog()
+        End With
+
+        If myFileDialog.FileName.ToString <> "" Then
+
+            Dim ExcelFile As String = myFileDialog.FileName.ToString
+
+            Dim ds As New DataSet
+            Dim da As OleDbDataAdapter
+            Dim dt As DataTable
+            Dim conn As OleDbConnection
+
+            xSheet = InputBox("Ingrese el nombre de la hoja a importar", "Complete")
+            conn = New OleDbConnection(
+                              "Provider=Microsoft.ACE.OLEDB.12.0;" &
+                              "data source=" & ExcelFile & "; " &
+                             "Extended Properties='Excel 12.0 Xml;HDR=Yes'")
+
+            Try
+                da = New OleDbDataAdapter("SELECT * FROM  [" & xSheet & "$]", conn)
+
+                conn.Open()
+                da.Fill(ds, "MyData")
+                dt = ds.Tables("MyData")
+                tabla.DataSource = ds
+                tabla.DataMember = "MyData"
+
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information, "Información")
+            Finally
+                conn.Close()
+            End Try
+
+            MsgBox("Se ha cargado la importación correctamente. ", MsgBoxStyle.Information, "Importado con exito")
+
+        Else
+
+            MsgBox("No se logro la importación.")
+
+        End If
+
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        importarExcel2(dtExcel)
+
+        lblCantidadR.Text = dtExcel.Rows.Count - 1
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+
+            M_ListaPrecios.dgbtabla.Rows.Clear()
+            For fila = 0 To dtExcel.Rows.Count - 2
+                M_ListaPrecios.dgbtabla.Rows.Add(New String() {"", "", dtExcel.Rows(fila).Cells(0).Value, dtExcel.Rows(fila).Cells(2).Value, dtExcel.Rows(fila).Cells(3).Value, dtExcel.Rows(fila).Cells(4).Value})
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Me.Close()
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub M_ImportarListasPrecios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        alternarColoFilasDatagridview(dtExcel)
+    End Sub
+End Class
